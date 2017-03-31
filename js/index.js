e3661419b4e4eda902c1f5ed9bb1d96b4a6bfc8a
kunalmehta12334 <kunalmehta2804@gmail.com>
2017-03-31 13:56:08 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.getJSON("api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",function(json){
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",function(json){
            console.log(json.quoteText);
     });
   }
