a3f975ef2bf60ada39f21ffc58b89c6604b6873c
kunalmehta12334 <kunalmehta2804@gmail.com>
2017-03-31 13:50:33 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $.getJSON("api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",function(json){
+           console.log(json.quoteText);
+    });
+  }
+);
